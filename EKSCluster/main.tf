b5412bb28db99d9cacd261b4c4ea8d45483e865e
sairam reddy <kasireddysairam03@gmail.com>
2024-04-10 18:58:31 +0530

    add to repo

--- EKSCluster/main.tf
+++ EKSCluster/main.tf
@@ -1,3 +1,16 @@
+
+locals {
+  cluster_name = "bms-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+
+
+
 #VPC
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -13,6 +26,7 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
+  enable_dns_support   = true
 
 
   tags = {
@@ -29,39 +43,36 @@ module "vpc" {
   }
 }
 
+
 #eks module
 
-
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.29"
+  enable_irsa = true
 
-  cluster_endpoint_public_access = true
+  tags = {
+    cluster = "demo"
+  }
 
+  vpc_id = module.vpc.vpc_id
 
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-
-
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t3.medium"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
 
   eks_managed_node_groups = {
 
-    nodes = {
-      min_size     = 1
-      max_size     = 3
+    node_group = {
+      min_size     = 2
+      max_size     = 6
       desired_size = 2
-
-      instance_type = ["t2.small"]
     }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
--- EKSCluster/variables.tf
+++ EKSCluster/variables.tf
@@ -14,3 +14,8 @@ variable "private_subnets" {
   type        = list(string)
 }
 
+variable "kubernetes_version" {
+  default     = 1.27
+  description = "kubernetes version"
+}
+
